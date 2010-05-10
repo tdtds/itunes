aeb2643f2c5891ea6ba06f6175028e8679157f41
TADA Tadashi <sho@katsuura.tdtds.jp>
2010-05-10 17:52:35 +0900

    added easy test.

--- lib/itunes.rb
+++ lib/itunes.rb
@@ -22,3 +22,21 @@ module ITunes
 		end
 	end
 end
+
+if __FILE__ == $0 then
+	begin
+		itunes = ITunes::App::new
+		pls = itunes.playlists
+		pls.each do |list|
+			print "#{list.name}: #{list.kind} - "
+			begin
+				puts list.SpecialKind
+			rescue WIN32OLERuntimeError
+				puts 'no special kind'
+			end
+		end
+	rescue WIN32OLERuntimeError
+		$stderr.puts $!
+		exit
+	end
+end
