ead9dd0429d21e15c7ebbc94fe5a8fc8472e888d
TADA Tadashi <sho@katsuura.tdtds.jp>
2010-05-11 18:25:56 +0900

    added playlist by name method.

--- lib/itunes.rb
+++ lib/itunes.rb
@@ -15,7 +15,9 @@ module ITunes
 			end
 		end
 
-	private
+		def playlist( name )
+			playlists.ItemByName( name ).extend( Playlist )
+		end
 
 		def playlists
 			pls = @itunes.LibrarySource.Playlists
@@ -25,9 +27,6 @@ module ITunes
 	end
 
 	module Playlists
-		def size
-			count
-		end
 	end
 
 	module Playlist
@@ -59,15 +58,26 @@ module ITunes
 		def to_symbol( kind_code )
 			KINDS[kind_code]
 		end
+
+		def count
+			self.Tracks.Count
+		end
 	end
 end
 
 if __FILE__ == $0 then
 	begin
 		itunes = ITunes::App::new
+		first = nil
 		itunes.each_playlist do |list|
 			puts "#{list.name}: #{list.to_symbol( list.kind )}"
+			first = list.name if list.kind == 0 and !first
 		end
+
+		puts
+		puts "Detail of #{first} list"
+		list = itunes.playlist( first )
+		puts "\tCount: #{list.count}"
 	rescue WIN32OLERuntimeError
 		$stderr.puts $!
 		exit
